e3d7476ad630c11979abf6e0644d416ab6fb17bd
Zakir Ahmad Khan <khanzakirkhan723595@gmail.com>
2026-07-01 01:03:17 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-6

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-6.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+   
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // stack<TreeNode*> st;
+        // st.push(root);
+        // while(!st.empty())
+        // {
+        //     TreeNode *node=st.top();
+        //     st.pop();
+            
+        //     if(p->val < node->val && q->val < node->val)
+        //     {
+        //         if(node->left!=NULL)
+        //             st.push(node->left);
+        //     }
+        //     else if(p->val > node->val && q->val > node->val)
+        //     {
+        //         if(node->right!=NULL)
+        //         {
+        //             st.push(node->right);
+        //         }
+
+        //     }
+        //     else{
+        //         return node;
+        //     }
+        // }
+        // return NULL;
+        
+        //method 2
+        TreeNode* cur = root;
+
+        while (cur) {
+            if (p->val > cur->val && q->val > cur->val) {
+                cur = cur->right;
+            } else if (p->val < cur->val && q->val < cur->val) {
+                cur = cur->left;
+            } else {
+                return cur;
+            }
+        }
+        return nullptr;
+    }
+};
