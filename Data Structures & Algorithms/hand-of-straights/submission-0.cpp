54ab1e9bf7fdd55f92ccf7d62619f718b4ff5ca3
Zakir Ahmad Khan <khanzakirkhan723595@gmail.com>
2026-08-10 00:45:35 +0530

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n=hand.size();
+        unordered_map<int,int>mp;
+        sort(hand.begin(),hand.end());
+        for(int i=0;i<n;i++)
+        {
+            mp[hand[i]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            
+            if(mp[hand[i]]>0)
+            {
+                int start=hand[i];
+                for(int j=0; j<groupSize; j++)
+                {
+                    int x = hand[i] + j;
+
+                    if(mp[x] <= 0)
+                        return false;
+
+                    mp[x]--;
+                }
+            }
+        }
+        return true;
+        
+    }
+};
