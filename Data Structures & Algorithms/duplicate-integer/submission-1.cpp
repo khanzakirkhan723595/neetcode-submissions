74614d1e8fb2986a6a2e1ceeed79a93764485d07
Zakir Ahmad Khan <khanzakirkhan723595@gmail.com>
2026-08-24 23:31:47 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        //sort(nums.begin(),nums.end());
+        unordered_set<int> st;
+        for(int i=0;i<n;i++)
+        {
+            if(st.count(nums[i]))
+            {
+                return true;
+            }
+            st.insert(nums[i]);
+            
+        }
+        return false;
+        
+    }
+};
